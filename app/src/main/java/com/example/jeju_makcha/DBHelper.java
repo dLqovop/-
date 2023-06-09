ac33a7991639d3936d7c0043bbbdf0d0d7d6d3d6
mmoiro <mipdjun824@gmail.com>
2023-06-09 14:38:40 +0900

    XX minutes saving

--- app/src/main/java/com/example/jeju_makcha/DBHelper.java
+++ app/src/main/java/com/example/jeju_makcha/DBHelper.java
@@ -13,9 +13,11 @@ import java.util.List;
 public class DBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "favorites_database.db";
     private static final int DATABASE_VERSION = 1;
-    private static final String TABLE_NAME = "favorites";
+    private static final String TABLE_NAME_FAVORITES = "favorites";
     private static final String COLUMN_ID = "id";
-    private static final String COLUMN_ITEM = "item";
+    private static final String COLUMN_FAVORITE_ITEM = "favorite_item";
+    private static final String TABLE_NAME_USER_SETTINGS = "user_settings";
+    private static final String COLUMN_MINUTES = "minutes";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -23,27 +25,35 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTableQuery = "CREATE TABLE " + TABLE_NAME + " (" +
+        String createFavoritesTableQuery = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME_FAVORITES + " (" +
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_ITEM + " TEXT" +
+                COLUMN_FAVORITE_ITEM + " TEXT" +
                 ")";
-        Log.i("DB", "onCreate 생성 확인");
-        db.execSQL(createTableQuery);
+        db.execSQL(createFavoritesTableQuery);
+
+        String createUserSettingsTableQuery = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME_USER_SETTINGS + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_MINUTES + " INTEGER" +
+                ")";
+        db.execSQL(createUserSettingsTableQuery);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String dropTableQuery = "DROP TABLE IF EXISTS " + TABLE_NAME;
-        db.execSQL(dropTableQuery);
+        String dropFavoritesTableQuery = "DROP TABLE IF EXISTS " + TABLE_NAME_FAVORITES;
+        db.execSQL(dropFavoritesTableQuery);
+
+        String dropUserSettingsTableQuery = "DROP TABLE IF EXISTS " + TABLE_NAME_USER_SETTINGS;
+        db.execSQL(dropUserSettingsTableQuery);
+
         onCreate(db);
     }
 
     public void insertFavorite(String item) {
-        SQLiteDatabase db = getReadableDatabase();
-        db = getWritableDatabase();
+        SQLiteDatabase db = getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(COLUMN_ITEM, item);
-        long newRowId = db.insert(TABLE_NAME, null, values);
+        values.put(COLUMN_FAVORITE_ITEM, item);
+        long newRowId = db.insert(TABLE_NAME_FAVORITES, null, values);
         if (newRowId == -1) {
             Log.e("DBHelper", "Failed to insert favorite item");
         } else {
@@ -54,10 +64,10 @@ public class DBHelper extends SQLiteOpenHelper {
     public List<String> getAllFavorites() {
         List<String> favorites = new ArrayList<>();
         SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.query(TABLE_NAME, null, null, null, null, null, null);
+        Cursor cursor = db.query(TABLE_NAME_FAVORITES, null, null, null, null, null, null);
         if (cursor != null) {
             while (cursor.moveToNext()) {
-                int itemColumnIndex = cursor.getColumnIndex(COLUMN_ITEM);
+                int itemColumnIndex = cursor.getColumnIndex(COLUMN_ID);
                 if (itemColumnIndex != -1) {
                     String item = cursor.getString(itemColumnIndex);
                     favorites.add(item);
@@ -67,4 +77,17 @@ public class DBHelper extends SQLiteOpenHelper {
         }
         return favorites;
     }
+
+    public void insertUserSetting(int minutes) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_MINUTES, minutes);
+        long newRowId = db.insert(TABLE_NAME_USER_SETTINGS, null, values);
+        if (newRowId == -1) {
+            Log.e("DBHelper", "Failed to insert user setting");
+        } else {
+            Log.i("DBHelper", "Inserted user setting: " + minutes);
+        }
+    }
 }
+
--- app/src/main/java/com/example/jeju_makcha/MainActivity.java
+++ app/src/main/java/com/example/jeju_makcha/MainActivity.java
@@ -1,43 +1,36 @@
 package com.example.jeju_makcha;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.text.Editable;
-import android.text.TextWatcher;
+
 import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {  // implements RecyclerViewAdapterCallback
 
     //바텀 네비게이션
     BottomNavigationView bottomNavigationView;
-    private String TAG = "메인페이지";
+    private final String TAG = "메인페이지";
 
     //프래그먼트 변수
     fragment_1 fragment_frag1;
     Fragment fragment_frag2;
     Fragment fragment_frag3;
 
-    private DBHelper dbHelper;
     private BusAdapter busAdapter;
-  /*
+
+    /*
     public void setBusAdapter(BusAdapter adapter) {
         busAdapter = adapter;
     }
@@ -63,35 +56,25 @@ public class MainActivity extends AppCompatActivity {  // implements RecyclerVie
 
         //리스너 등록
         bottomNavigationView.setOnItemSelectedListener(item -> {
-                Log.i(TAG,"바텀네비게이션 클릭");
-                int itemId = item.getItemId();//item을 클릭시 id값을 가져와 FrameLayout에 fragment.xml띄우기
-                if (itemId == R.id.page_1) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,fragment_frag1).commitAllowingStateLoss();
-                    return true;
-                }
-                else if (itemId == R.id.page_2) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,fragment_frag2).commitAllowingStateLoss();
-                    return true;
-                }
-                else if (itemId == R.id.page_3) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,fragment_frag3).commitAllowingStateLoss();
-                    return true;
-                }
+            Log.i(TAG, "바텀네비게이션 클릭");
+            int itemId = item.getItemId();//item을 클릭시 id값을 가져와 FrameLayout에 fragment.xml띄우기
+            if (itemId == R.id.page_1) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment_frag1).commitAllowingStateLoss();
                 return true;
-            });
+            } else if (itemId == R.id.page_2) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment_frag2).commitAllowingStateLoss();
+                return true;
+            } else if (itemId == R.id.page_3) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment_frag3).commitAllowingStateLoss();
+                return true;
+            }
+            return true;
+        });
 
         // Frag1을 초기화면으로 설정
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.main_frame, new fragment_1());
         fragmentTransaction.commit();
-
-        dbHelper = new DBHelper(this); // DBHelper 인스턴스 생성
-
-        // DB에 저장된 값 로그로 출력
-        List<String> favorites = dbHelper.getAllFavorites();
-        for (String item : favorites) {
-            Log.i("DBHelper", "Favorite MainActivity: " + item);
-        }
     }
 }
