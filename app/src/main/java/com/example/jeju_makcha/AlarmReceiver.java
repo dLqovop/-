55f4fe2153b6f0ca13d2cbabc8cd13154bed39e7
dLqovop <aaa8994@naver.com>
2023-06-16 02:52:10 +0900

    알람

---
+++ app/src/main/java/com/example/jeju_makcha/AlarmReceiver.java
@@ -0,0 +1,53 @@
+package com.example.jeju_makcha;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private static final String CHANNEL_ID = "MyNotificationChannel";
+    private static final int NOTIFICATION_ID = 1;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // 알림 생성 및 표시
+        createNotificationChannel(context);
+        showNotification(context);
+    }
+
+    private void createNotificationChannel(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void showNotification(Context context) {
+
+        // 알림 클릭 시 실행될 액티비티 지정
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // 알림 생성
+        Notification.Builder builder = new Notification.Builder(context, CHANNEL_ID)
+                .setContentTitle("버스 출발하기 전입니다.")
+                .setContentText("버스 시간표를 확인하세요.")
+                .setSmallIcon(R.drawable.logo_blue)
+                .setContentIntent(pendingIntent) // 클릭 이벤트 설정
+                .setAutoCancel(true); // 클릭 후 알림이 자동으로 사라지도록 설정
+
+        // 알림 표시
+        NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+        if (notificationManager != null) {
+            notificationManager.notify(NOTIFICATION_ID, builder.build());
+        }
+    }
+}
--- app/src/main/java/com/example/jeju_makcha/fragment_3.java
+++ app/src/main/java/com/example/jeju_makcha/fragment_3.java
@@ -1,5 +1,9 @@
 package com.example.jeju_makcha;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,26 +19,42 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class fragment_3 extends Fragment {
+    private static final int PERMISSION_REQUEST_CODE = 1;
     private EditText minutesEditText;
     private Button settingsButton;
     private DBHelper dbHelper;
+    private AlarmManager alarmManager;
+    private PendingIntent alarmIntent;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_frag3, container, false);
 
+
         minutesEditText = view.findViewById(R.id.minuteEditText);
         settingsButton = view.findViewById(R.id.settingsButton);
         dbHelper = new DBHelper(getActivity());
-
+        alarmManager = (AlarmManager) requireContext().getSystemService(Context.ALARM_SERVICE);
+        Intent alarmReceiverIntent = new Intent(requireContext(), AlarmReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(requireContext(), 0, alarmReceiverIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         settingsButton.setOnClickListener(v -> {
             String minutesString = minutesEditText.getText().toString();
             if (!minutesString.isEmpty()) {
                 int minutes = Integer.parseInt(minutesString);
-                SQLiteDatabase db = dbHelper.getWritableDatabase(); // 또는 getReadableDatabase()
-                //dbHelper.onCreate(db);
+                long delayMillis = minutes * 60 * 1000; // 분을 밀리초로 변환
+
+                // 이전에 설정된 알람을 취소
+                alarmManager.cancel(alarmIntent);
+
+                // 현재 시간에 delayMillis를 더하여 알람 설정
+                long triggerTimeMillis = System.currentTimeMillis() + delayMillis;
+                Log.d("시간","시간 : "+triggerTimeMillis);
+                alarmManager.set(AlarmManager.RTC_WAKEUP, triggerTimeMillis, alarmIntent);
+
+                // 사용자 설정 저장
                 dbHelper.insertUserSetting(minutes);
+
                 Toast.makeText(getActivity(), "설정이 저장되었습니다.", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(getActivity(), "분을 입력해주세요.", Toast.LENGTH_SHORT).show();
@@ -44,4 +64,3 @@ public class fragment_3 extends Fragment {
         return view;
     }
 }
-
